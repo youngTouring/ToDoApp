c9a00ea8cc9e7bee1dfd4b2e9c259ed89826a60e
Mikolaj <mikolajdara@gmail.com>
2021-05-16 22:28:08 +0200

    Input task

--- src/components/AddTask.js
+++ src/components/AddTask.js
@@ -4,11 +4,30 @@ import './AddTask.css';
 
 
 class AddTask extends Component {
-  state = {  }
+  
+  minDate = new Date().toISOString().slice(0,10)
+  state = {  
+    text: '',
+    checked: false,
+    date: this.minDate
+  }
+
+  handleDate = (e) => {
+    this.setState({
+      date: e.target.value
+    })
+  }
   render() { 
+    let maxDate = this.minDate.slice(0,4) * 1 + 1;
+    maxDate = maxDate + '-12-31'
     return (
       <div className = 'form'>
-        <div>Dodaj zadanie</div>
+        <input type = 'text' placeholder = 'dodaj zadanie' value = {this.state.text}></input>
+        <input type = 'checkbox' checked = {this.state.checked} id = 'important'></input>
+        <label htmlFor = 'important'>Priorytet</label><br/>
+        <label htmlFor = 'date'>Do kiedy zrobić</label>
+        <input type = 'date' value = {this.state.date} min = {this.minDate} max = {maxDate} onChange = {this.handleDate}></input>
+        <button>Dodaj</button>
         <hr></hr>
       </div> 
       
